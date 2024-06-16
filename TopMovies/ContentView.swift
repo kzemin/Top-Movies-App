ee14382c8763bedd9dd29ce740299f91e61eb6ad
Facundo Kzemin Paez <facupaezk@gmail.com>
2024-06-16 20:51:39 -0300

    Initial Commit

---
+++ TopMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TopMovies
+//
+//  Created by Facundo Kzemin on 16/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TopMovies/TopMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopMoviesApp.swift
+//  TopMovies
+//
+//  Created by Facundo Kzemin on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TopMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
